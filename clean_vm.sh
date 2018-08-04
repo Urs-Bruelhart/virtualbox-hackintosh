b7f0a83c82487fe1a61858e6e7455f1db959bed2
brad2014 <brad2014@users.noreply.github.com>
2018-08-03 22:17:48 -0700

    delete vm files during cleanup

--- clean_vm.sh
+++ clean_vm.sh
@@ -5,5 +5,5 @@ VM="MacOS High Sierra"
 set -e
 set -x
 VBoxManage controlvm "$VM" poweroff 2>/dev/null || :
-VBoxManage unregistervm "$VM" 2>/dev/null || :
+VBoxManage unregistervm "$VM" --delete 2>/dev/null || :
 rm -f macos-hs-live.vdi
